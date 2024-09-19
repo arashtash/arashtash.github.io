ecd80cfc3988dba56ac0e4b20c86f300e4a89cab
Arash Tashakori <140542600+arashtash@users.noreply.github.com>
2024-09-19 20:12:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,9 @@ document.getElementById('toggleCourses').addEventListener('click', function() {
     this.textContent = isHidden ? 'Show Less' : 'View All';
 });
 
-if (navigator.userAgent.includes('iPhone')) {
-    document.querySelector('meta[name="viewport"]').setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
+if(navigator.userAgent.indexOf('iPhone') > -1 )
+{
+    document
+      .querySelector("[name=viewport]")
+      .setAttribute("content","width=device-width, initial-scale=1, maximum-scale=1");
 }
